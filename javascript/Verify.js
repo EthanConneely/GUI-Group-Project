f4660ed58d5df391ed4965d2dd1cf0ee3e1ba2e1
caolanmichaelmaguire <36414229+caolanmichaelmaguire@users.noreply.github.com>
2021-04-14 23:59:52 +0100

    Added in Verify File

---
+++ javascript/Verify.js
@@ -0,0 +1,8 @@
+//Javascript Verify Script
+sessionStorage.setItem('username','Caolan');
+
+if (sessionStorage.getItem("username") === null) {
+  alert('User is not logged in - redirecting to login page');
+}else{
+	document.getElementById('UsernameTag').innerHTML = 'Welcome, ' + sessionStorage.getItem('username');
+}
