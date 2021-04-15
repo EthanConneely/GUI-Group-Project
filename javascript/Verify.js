9e39a9bb3486e98b26090a0be39ac3b39aff8956
caolanmichaelmaguire <36414229+caolanmichaelmaguire@users.noreply.github.com>
2021-04-15 21:03:28 +0100

    Update Verify.js

--- javascript/Verify.js
+++ javascript/Verify.js
@@ -1,5 +1,6 @@
 //Javascript Verify Script
 
+<<<<<<< Updated upstream
 if (localStorage.getItem("username") === null) {
   alert('User is not logged in');
   location.href = 'login.html';
@@ -13,4 +14,28 @@ if (localStorage.getItem("username") === null) {
 function LogoutFunction(){
 	localStorage.clear();
 	location.href = 'login.html';
+=======
+//Verifying if user is logged in - if not sending them to login page
+if (sessionStorage.getItem("username") === null) {
+  alert('User is not logged in - redirecting to login page');
+  //window.location.href = '';
+}else{
+	document.getElementById('UsernameTag').innerHTML = 'Welcome, ' + sessionStorage.getItem('username');
+}
+
+function LoginQuery(username,password){
+	
+	PossibleUsernames = ['caolanmichaelmaguire','irishbruise'];
+	
+	UserCredentials = {
+		'caolanmichaelmaguire' : 'password123',
+		'irishbruise' : 'password1234'
+	}
+	
+	if(PossibleUsernames.includes(username)){
+		//if(UserCredentials[username]){
+		//}
+	}
+	
+>>>>>>> Stashed changes
 }
