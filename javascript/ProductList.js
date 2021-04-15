9ab1cd5d64268405ebc82cc37aafd36c77c67238
caolanmichaelmaguire <36414229+caolanmichaelmaguire@users.noreply.github.com>
2021-04-15 17:32:33 +0100

    ProductList.js File Created and added under /js

---
+++ javascript/ProductList.js
@@ -0,0 +1,12 @@
+ProductsList = [
+        ['01', 'images/product1.png', 'First Product', 'This is the description of the first product', '52'],
+		['02', 'images/product2.png', 'Second Product', 'This is the description of the second product', '62'],
+		['03', 'images/product3.png', 'Third Product', 'This is the description of the third product', '72'],
+		['04', 'images/product4.png', 'Fourth Product', 'This is the description of the fourth product', '82'],
+		['05', 'images/product5.png', 'Fifth Product', 'This is the description of the fifth product', '92'],
+		['06', 'images/product6.png', 'Sixth Product', 'This is the description of the sixth product', '102'],
+		['07', 'images/product7.png', 'Seventh Product', 'This is the description of the seventh product', '112'],
+		['08', 'images/product8.png', 'Eighth Product', 'This is the description of the eighth product', '122'],
+		['09', 'images/product9.png', 'Ninth Product', 'This is the description of the ninth product', '132'],
+		['10', 'images/product10.png', 'Tenth Product', 'This is the description of the tenth product', '142']
+    ];
