de0ce150891a67bee18ce097982e0f6f4bd90d9c
EthanConneely <G00393941@GMIT.ie>
2021-04-21 00:11:05 +0100

    Add comments

--- Store/javascript/BasketBadge.js
+++ Store/javascript/BasketBadge.js
@@ -1,14 +1,14 @@
+// refresh the badge so it is up to date (EC)
 window.setInterval(() => RefreshBadge(), 250);
 
-if (sessionStorage.cart != undefined)
-{
-    RefreshBadge();
-}
-
-
+// Refresh the badge number (EC)
 function RefreshBadge()
 {
-    let badge = document.getElementById("BasketBadge");
-    badge.innerText = sessionStorage.cart.split(',').length;
-    badge.style.display = "block";
+    // no need to show the badge if there is nothing added to the cart (EC)
+    if (sessionStorage.cart != undefined)
+    {
+        let badge = document.getElementById("BasketBadge");
+        badge.innerText = sessionStorage.cart.split(',').length;
+        badge.style.display = "block";
+    }
 }
