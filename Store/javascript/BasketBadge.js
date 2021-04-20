062d11e3220537637898026472a9ed5e4f32a8d9
EthanConneely <G00393941@GMIT.ie>
2021-04-21 00:03:43 +0100

    Add refresh to badge so it is kept up to date

--- Store/javascript/BasketBadge.js
+++ Store/javascript/BasketBadge.js
@@ -1,7 +1,14 @@
-let badge = document.getElementById("BasketBadge");
+window.setInterval(() => RefreshBadge(), 250);
 
 if (sessionStorage.cart != undefined)
 {
+    RefreshBadge();
+}
+
+
+function RefreshBadge()
+{
+    let badge = document.getElementById("BasketBadge");
     badge.innerText = sessionStorage.cart.split(',').length;
     badge.style.display = "block";
 }
