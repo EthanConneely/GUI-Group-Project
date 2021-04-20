eb971c379c9331925a99dd695d8426a7a1862952
caolanmichaelmaguire <36414229+caolanmichaelmaguire@users.noreply.github.com>
2021-04-20 12:05:02 +0100

    Header & Product Images added

--- javascript/ProductList.js
+++ javascript/ProductList.js
@@ -1,12 +1,10 @@
 Products = [
-    [1, 'images/Temp.png', 'First Product', 'This is the description of the first product', '€52.00'],
-    [2, 'images/Temp.png', 'Second Product', 'This is the description of the second product', '€62.00'],
-    [3, 'images/Temp.png', 'Third Product', 'This is the description of the third product', '€72.00'],
-    [4, 'images/Temp.png', 'Fourth Product', 'This is the description of the fourth product', '€82.00'],
-    [5, 'images/Temp.png', 'Fifth Product', 'This is the description of the fifth product', '€92.00'],
-    [6, 'images/Temp.png', 'Sixth Product', 'This is the description of the sixth product', '€102.00'],
-    [7, 'images/Temp.png', 'Seventh Product', 'This is the description of the seventh product', '€112.00'],
-    [8, 'images/Temp.png', 'Eighth Product', 'This is the description of the eighth product', '€122.00'],
-    [9, 'images/Temp.png', 'Ninth Product', 'This is the description of the ninth product', '€132.00'],
-    [10, 'images/Temp.png', 'Tenth Product', 'This is the description of the tenth product', '€142.00']
+    [1, 'images/image1.jpg', 'A Shoe', 'This is the description of the first product', '€52.00'],
+    [2, 'images/image2.jpg', 'A Shoe', 'This is the description of the second product', '€62.00'],
+    [3, 'images/image3.jpg', 'A Shoe', 'This is the description of the third product', '€72.00'],
+    [4, 'images/image4.jpg', 'A Shoe', 'This is the description of the fourth product', '€82.00'],
+    [5, 'images/image5.jpg', 'A Shoe', 'This is the description of the fifth product', '€92.00'],
+    [6, 'images/image6.jpg', 'A Shoe', 'This is the description of the sixth product', '€102.00'],
+    [7, 'images/image7.jpg', 'A Shoe', 'This is the description of the seventh product', '€112.00'],
+    [8, 'images/image8.jpg', 'A Shoe', 'This is the description of the eighth product', '€122.00']
 ];
