111483f0cd0b42a1761f36df344bd9395bc67823
EthanConneely <G00393941@GMIT.ie>
2021-04-22 00:04:54 +0100

    Footer fixed again

---
+++ Cart/javascript/FooterFix.js
@@ -0,0 +1,13 @@
+let bodyHeight = document.getElementById("body").offsetHeight;
+let viewportHeight = window.innerHeight;
+
+offset = viewportHeight - bodyHeight + 20;
+alert(offset);
+if (offset < 0)
+{
+    document.getElementById("footer").style.marginTop = "0px";
+}
+else
+{
+    document.getElementById("footer").style.marginTop = offset + "px";
+}
